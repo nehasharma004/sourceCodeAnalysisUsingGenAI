759fadedc5285062c1e55bc6b059e102740ba62a
nehasharma004 <163660958+nehasharma004@users.noreply.github.com>
2024-04-05 12:02:30 +0530

    Add files via upload

---
+++ Source-Code-Analysis-using-GenAI-main/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Neha Sharma',
+    author_email= 'neha04021990sharma@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
